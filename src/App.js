9ccd854e49e1ac770f0e8fec33e5e51c18becbf3
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-05-29 20:45:48 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react";
+import Home from './components/Home/Home';
+import Verificar from './components/Verificar/Verificar';
+import NavbarFunction from './components/Navbar/Navbar';
+
 import './App.css';
 
 function App() {
+  const [onHome, selectedHome] = useState(true);
+  const [onVerificar, selectedVerificar] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+     
+     <NavbarFunction  
+          onHome={onHome}selectedHome={selectedHome}
+          onVerificar={onVerificar}selectedVerificar={selectedVerificar}
+          />
+          {/* If variable == true show that component */}
+          {onHome ? (<Home />):("")}
+          {onVerificar ? (<Verificar/>):("")}
+     </section>
+     
+
   );
 }
 
